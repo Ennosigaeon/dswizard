91e9ff3cf703474eee3aef324913fb48cd5ed003
Marc Zoeller <m.zoeller@usu.de>
2020-05-04 22:00:28 +0200

    Load requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='dswizard',
     version='0.1.0',
     description='DataScience Wizard for automatic assembly of machine learning pipelines',
     author='Marc Zoeller',
     author_email='m.zoeller@usu.de',
-    url="https://gitlab.usu-research.ml/research/automl/hpbandster",
+    url="https://gitlab.usu-research.ml/research/automl/dswizard",
     license='BSD 3-Clause License',
     classifiers=['Development Status :: 4 - Beta'],
     packages=find_packages(),
     python_requires='>=3',
-    install_requires=['Pyro4', 'serpent', 'ConfigSpace', 'numpy', 'statsmodels', 'scipy', 'netifaces', 'scikit-learn',
-                      'IPython', 'matplotlib', 'pandas'],
+    install_requires=requirements,
     extras_require={
         'docu': ['sphinx', 'sphinx_rtd_theme', 'sphinx_gallery'],
     },
