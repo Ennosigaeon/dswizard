d1716b13965014b8b68c4a34b3b9b92644c45998
Marc Zoeller <m.zoeller@usu.de>
2020-09-18 11:18:53 +0200

    Use OpenML tasks instead of datasets

--- examples/example_1_local.py
+++ examples/example_1_local.py
@@ -8,11 +8,7 @@ import logging
 import sys
 
 import openml
-import sklearn
 
-from automl.components.classification.linear_discriminant_analysis import LinearDiscriminantAnalysis
-from automl.components.data_preprocessing.robust_scaler import RobustScalerComponent
-from automl.components.feature_preprocessing.one_hot_encoding import OneHotEncoderComponent
 from dswizard.core.logger import JsonResultLogger
 from dswizard.core.master import Master
 from dswizard.core.model import Dataset
@@ -31,38 +27,36 @@ parser = argparse.ArgumentParser(description='Example 1 - sequential and local e
 parser.add_argument('--min_budget', type=float, help='Minimum budget used during the optimization.', default=1)
 parser.add_argument('--max_budget', type=float, help='Maximum budget used during the optimization.', default=10)
 parser.add_argument('--n_configs', type=float, help='Number of configurations to test on a single structure', default=1)
-parser.add_argument('--wallclock_limit', type=float, help='Maximum optimization time for in seconds', default=60)
+parser.add_argument('--wallclock_limit', type=float, help='Maximum optimization time for in seconds', default=300)
 parser.add_argument('--cutoff', type=float, help='Maximum cutoff time for a single evaluation in seconds', default=60)
 parser.add_argument('--run_id', type=str, help='Name of the run', default='run')
 parser.add_argument('--log_dir', type=str, help='Directory used for logging', default='run/logs/')
 args = parser.parse_args()
 
 # Load dataset
-ds = openml.datasets.get_dataset(23381)
-X, y, categorical_indicator, attribute_names = ds.get_data(target=ds.default_target_attribute)
-X, y = sklearn.utils.shuffle(X.to_numpy(), y.to_numpy())
-ds = Dataset(X, y)
+task = openml.tasks.get_task(18)
+train_indices, test_indices = task.get_train_test_split_indices(repeat=0, fold=0, sample=0)
 
-steps = [
-    ('1', DataPreprocessorChoice()),
-    # ('2', FeaturePreprocessorChoice()),
-    ('3', DecisionTree())
-]
+# noinspection PyUnresolvedReferences
+X, y, _, _ = task.get_dataset().get_data(task.target_name)
+X_train = X.loc[train_indices]
+y_train = y[train_indices]
+X_test = X.loc[test_indices]
+y_test = y[test_indices]
+
+ds = Dataset(X_train.to_numpy(), y_train.to_numpy())
 
 master = Master(
     ds=ds,
     run_id=args.run_id,
     result_logger=JsonResultLogger(directory=args.log_dir, overwrite=True),
-    n_workers=1,
+    n_workers=4,
 
     wallclock_limit=args.wallclock_limit,
     cutoff=args.cutoff,
     pre_sample=False,
 
     config_generator_class=Hyperopt,
-
-    # structure_generator_class=FixedStructure,
-    # structure_generator_kwargs={'steps': steps},
 
     bandit_learner_class=HyperbandLearner,
     bandit_learner_kwargs={'min_budget': args.min_budget,
@@ -72,26 +66,20 @@ master = Master(
 try:
     pipeline, run_history = master.optimize()
 
-    pipeline.fit(X, y)
-    predicitions = pipeline.predict(X)
-
     # Analysis
     id2config = run_history.get_id2config_mapping()
     _, incumbent = run_history.get_incumbent()
-
-    if logging.getLogger().level <= logging.DEBUG:
-        print('Tested configurations:')
-        for cid, structure in id2config.items():
-            print('{}\t -> {}'.format(cid, structure.steps))
-            for res in structure.results:
-                print('\t{} -> {}'.format(res.config, res.loss))
 
     print('Best found configuration: {}\n{} with loss {}'.format(incumbent.steps,
                                                                  incumbent.get_incumbent().config,
                                                                  incumbent.get_incumbent().loss))
     print('A total of {} structures where sampled.'.format(len(id2config.keys())))
     print('A total of {} runs where executed.'.format(len(run_history.get_all_runs())))
-    print('Final performance', util.score(y, predicitions, ds.metric))
+
+    print('Final pipeline:\n{}'.format(pipeline))
+    pipeline.fit(X, y)
+    predictions = pipeline.predict(X_test)
+    print('Final test performance', util.score(y_test, predictions, ds.metric))
 
 finally:
     master.shutdown()
