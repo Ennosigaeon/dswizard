0cbf3b2722eb27806b23c6beb401df6160f09afa
Marc Zoeller <m.zoeller@usu.de>
2020-10-02 18:17:40 +0200

    Load trajectories for all tasks/folds

--- scripts/load_trajectories.py
+++ scripts/load_trajectories.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 import numpy as np
 from scipy.stats import rankdata
@@ -6,37 +7,51 @@ from smac.runhistory.runhistory import RunHistory
 
 import matplotlib.pyplot as plt
 
+min_time = 0
+max_time = 3660
 
-def load_autosklearn():
-    with open('autosklearn/configspace.pkl', 'rb') as f:
-        cs = pickle.load(f)
+worst_score = (0, 1)
+factor = 1
 
-    with open('autosklearn/start.txt', 'r') as f:
-        start = float(f.readline())
 
-    rh = RunHistory(lambda x: x)
-    rh.load_json('autosklearn/runhistory_0.json', cs)
-
-    scores = []
-    for value in rh.data.values():
-        scores.append((value.additional_info + value.time - start, value.cost))
-    scores.insert(0, (0, 0))
+def _format(scores):
+    scores.insert(0, (min_time, worst_score[0]))
     scores = np.array(scores)
-    scores[:, 1] = np.maximum.accumulate(scores[:, 1])
+    scores[:, 1] = np.clip(scores[:, 1], min(*worst_score), max(*worst_score))
+    scores[:, 1] = factor * scores[:, 1]
+    # scores[:, 1] = np.maximum.accumulate(scores[:, 1])
     return scores
 
 
-def load_tpot():
-    with open('tpot/runhistory.pkl', 'rb') as f:
-        rh = pickle.load(f)
+def load_autosklearn(base_dir: str):
+    try:
+        with open(os.path.join(base_dir, 'configspace.pkl'), 'rb') as f:
+            cs = pickle.load(f)
 
-    scores = []
-    for time, score, pipeline in rh:
-        scores.append((time, score))
-    scores.insert(0, (0, 0))
-    scores = np.array(scores)
-    scores[:, 1] = np.maximum.accumulate(scores[:, 1])
-    return scores
+        with open(os.path.join(base_dir, 'start.txt'), 'r') as f:
+            start = float(f.readline())
+
+        rh = RunHistory(lambda x: x)
+        rh.load_json(os.path.join(base_dir, 'runhistory_0.json'), cs)
+        scores = []
+        for value in rh.data.values():
+            scores.append((value.additional_info + value.time - start, value.cost))
+    except FileNotFoundError:
+        scores = [(min_time, worst_score[0]), (max_time, worst_score[0])]
+
+    return _format(scores)
+
+
+def load_tpot(base_dir):
+    try:
+        with open(os.path.join(base_dir, 'runhistory.pkl'), 'rb') as f:
+            rh = pickle.load(f)
+            scores = []
+            for time, score, pipeline in rh:
+                scores.append((time, score))
+    except FileNotFoundError:
+        scores = [(min_time, worst_score[0]), (max_time, worst_score[0])]
+    return _format(sorted(scores))
 
 
 def compute_rank(data):
@@ -54,19 +69,35 @@ def compute_rank(data):
     return rank
 
 
-auto_sklearn = load_autosklearn()
-tpot = load_tpot()
+for task, metric in [('ada_agnostic', 'auc'),
+                     ('adult', 'auc'),
+                     ('analcatdata_authorship', 'logloss'),
+                     ('analcatdata_dmft', 'logloss'),
+                     ('Australian', 'auc'),
+                     ('eeg-eye-state', 'auc'),
+                     ('mfeat-morphological', 'logloss'),
+                     ('segment', 'logloss'),
+                     ('sylvine', 'auc'),
+                     ('vehicle', 'logloss')]:
+    if metric == 'logloss':
+        factor = -1
+        worst_score = (5, 0)
+    else:
+        factor = 1
+        worst_score = (0, 1)
 
-x_max = max(auto_sklearn[:, 0].max(), tpot[:, 0].max())
-auto_sklearn = np.vstack((auto_sklearn, np.array([[x_max, auto_sklearn[-1, 1]]])))
-tpot = np.vstack((tpot, np.array([[x_max, tpot[-1, 1]]])))
+    for fold in range(10):
+        auto_sklearn = load_autosklearn(os.path.join('/mnt/c/local/results/combined/autosklearn/', task, str(fold)))
+        tpot = load_tpot(os.path.join('/mnt/c/local/results/combined/tpot/', task, str(fold)))
 
-rank = compute_rank([auto_sklearn, tpot])
+        rank = compute_rank([auto_sklearn, tpot])
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-ax.step(auto_sklearn[:, 0], auto_sklearn[:, 1], label='auto-sklearn')
-ax.step(tpot[:, 0], tpot[:, 1], label='tpot')
-ax.legend()
-fig.savefig('fig/trajectory.png')
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(auto_sklearn[:, 0], auto_sklearn[:, 1], label='auto-sklearn')
+        ax.step(auto_sklearn[:, 0], np.maximum.accumulate(auto_sklearn[:, 1]), label='auto-sklearn', where='post')
+        ax.scatter(tpot[:, 0], tpot[:, 1], label='tpot')
+        ax.step(tpot[:, 0], np.maximum.accumulate(tpot[:, 1]), label='tpot', where='post')
+        ax.legend()
+        fig.savefig('fig/{}_{}.png'.format(task, fold))
+        plt.close(fig)
