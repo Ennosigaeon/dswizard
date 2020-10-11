be666bc33b14c89239bc689d76b4919a54fe7dfd
Marc Zoeller <m.zoeller@usu.de>
2020-10-11 18:35:46 +0200

    Create fixed structure in fill_candidate to have access to MF

--- dswizard/optimizers/structure_generators/fixed.py
+++ dswizard/optimizers/structure_generators/fixed.py
@@ -1,8 +1,5 @@
 from typing import List, Tuple
 
-import numpy as np
-from ConfigSpace.configuration_space import ConfigurationSpace
-
 from automl.components.base import ComponentChoice, EstimatorComponent
 from dswizard.components.pipeline import FlexiblePipeline
 from dswizard.core.base_structure_generator import BaseStructureGenerator
@@ -14,25 +11,20 @@ class FixedStructure(BaseStructureGenerator):
 
     def __init__(self, steps: List[Tuple[str, EstimatorComponent]], cfg_cache: ConfigCache, **kwargs):
         super().__init__(cfg_cache=cfg_cache, **kwargs)
-        self.configspace = ConfigurationSpace()
-        self.cfg_keys: List[Tuple[float, int]] = []
+        self.steps = steps
 
-        length = 0
-        for step, task in steps:
+    def fill_candidate(self, cs: CandidateStructure, ds: Dataset, **kwargs) -> CandidateStructure:
+        cfg_keys: List[Tuple[float, int]] = []
+        for step, task in self.steps:
             if isinstance(task, ComponentChoice) or isinstance(task, EstimatorComponent):
-                cg, key = cfg_cache.get_config_generator(configspace=task.get_hyperparameter_search_space(),
-                                                         mf=np.ones((1, 1)) * length)
+                cg, key = self.cfg_cache.get_config_generator(configspace=task.get_hyperparameter_search_space(),
+                                                              mf=ds.meta_features)
                 cs = task.get_hyperparameter_search_space()
             else:
                 raise ValueError('Unable to handle type {}'.format(type(task)))
-            self.configspace.add_configuration_space(step, cs)
-            self.cfg_keys.append(key)
-            length += 1
+            cfg_keys.append(key)
 
-        self.pipeline = FlexiblePipeline(steps)
-
-    def fill_candidate(self, cs: CandidateStructure, ds: Dataset, **kwargs) -> CandidateStructure:
-        cs.configspace = self.configspace
-        cs.pipeline = self.pipeline
-        cs.cfg_keys = self.cfg_keys
+        cs.pipeline = FlexiblePipeline(self.steps)
+        cs.configspace = cs.pipeline.get_hyperparameter_search_space(ds.meta_features)
+        cs.cfg_keys = cfg_keys
         return cs
