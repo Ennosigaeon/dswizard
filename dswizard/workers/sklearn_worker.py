bc93e641ed5a4e6ad685d11a8ba1f4ada0c4bec2
Marc Zoeller <m.zoeller@usu.de>
2021-07-21 17:10:06 +0200

    Quick and dirty implementation of holdout datasets

--- dswizard/workers/sklearn_worker.py
+++ dswizard/workers/sklearn_worker.py
@@ -7,7 +7,7 @@ import numpy as np
 from ConfigSpace import Configuration
 from sklearn import clone
 from sklearn.base import is_classifier
-from sklearn.model_selection import check_cv
+from sklearn.model_selection import check_cv, train_test_split
 from sklearn.model_selection._validation import _fit_and_predict, _check_is_permutation
 from sklearn.utils import indexable
 from sklearn.utils.validation import _num_samples
@@ -53,10 +53,11 @@ class SklearnWorker(Worker):
         component.set_hyperparameters(config.get_dictionary())
         if is_classifier(component):
             score, y_pred, y_prob, models = self._score(ds, component)
-            try:
-                y_pred = y_pred.astype(float)
-            except ValueError:
-                pass
+            # TODO crude fix for holdout score. Fix this
+            if y_pred.shape != ds.y.shape:
+                y_pred = models[0].predict(ds.X)
+                y_prob = models[0].predict_proba(ds.X)
+
             X = np.hstack((ds.X, y_prob, np.reshape(y_pred, (-1, 1))))
         else:
             models = [component.fit(ds.X, ds.y)]
@@ -65,18 +66,35 @@ class SklearnWorker(Worker):
         self._store_models(cid, models)
         return X, score
 
-    def _score(self, ds: Dataset, estimator: Union[EstimatorComponent, FlexiblePipeline], n_folds: int = 4):
-        y = ds.y
-        y_pred, y_prob, models = self._cross_val_predict(estimator, ds.X, y, cv=n_folds)
+    def _score(self, ds: Dataset, estimator: Union[EstimatorComponent, FlexiblePipeline], use_cv: bool = False)\
+            -> Tuple[List[float], np.ndarray, np.ndarray, List[FlexiblePipeline]]:
+        # TODO improve handling of holdout or cross-val prediction
+        if use_cv:
+            y, y_pred, y_prob, models = self._cross_val_predict(estimator, ds.X, ds.y, cv=4)
+        else:
+            y, y_pred, y_prob, models = self._holdout_predict(estimator, ds.X, ds.y)
 
         # Meta-learning only considers f1. Calculate f1 score for structure search
         score = [util.score(y, y_prob, y_pred, ds.metric), util.score(y, y_prob, y_pred, 'f1')]
         return score, y_pred, y_prob, models
 
     @staticmethod
-    def _cross_val_predict(pipeline, X, y=None, cv=None):
+    def _holdout_predict(pipeline, X, y=None, test_size=0.2)\
+            -> Tuple[np.ndarray, np.ndarray, np.ndarray, List[FlexiblePipeline]]:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+        cloned_pipeline: FlexiblePipeline = clone(pipeline)
+        cloned_pipeline.fit(X_train, y_train)
+        y_pred = cloned_pipeline.predict(X_test)
+        y_prob = cloned_pipeline.predict_proba(X_test)
+        return y_test, y_pred, y_prob, [cloned_pipeline]
+
+
+    @staticmethod
+    def _cross_val_predict(pipeline, X, y=None, cv=None)\
+            -> Tuple[np.ndarray, np.ndarray, np.ndarray, List[FlexiblePipeline]]:
         X, y, groups = indexable(X, y, None)
         cv = check_cv(cv, y, classifier=is_classifier(pipeline))
+        cv.random_state = 42
 
         prediction_blocks = []
         probability_blocks = []
@@ -104,12 +122,12 @@ class SklearnWorker(Worker):
         predictions = np.concatenate(predictions)
 
         if isinstance(predictions, list):
-            return [p[inv_test_indices] for p in predictions], [p[inv_test_indices] for p in
+            return y, [p[inv_test_indices] for p in predictions], [p[inv_test_indices] for p in
                                                                 probabilities], fitted_pipelines
         else:
-            return predictions[inv_test_indices], probabilities[inv_test_indices], fitted_pipelines
+            return y, predictions[inv_test_indices], probabilities[inv_test_indices], fitted_pipelines
 
-    def _store_models(self, cid: CandidateId, models: List[EstimatorComponent]):
+    def _store_models(self, cid: CandidateId, models: List[Union[EstimatorComponent, FlexiblePipeline]]):
         name = model_file(cid)
         file = os.path.join(self.workdir, name)
         with open(file, 'wb') as f:
